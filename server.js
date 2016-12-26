b6d6396dfdd25c01bb086bf7ada0923d3d6d26c6
Steven Martin <steven@stevenmartin.io>
2016-12-26 17:50:24 -0500

    Adjust caps on variable to read the correct thing for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 ﻿var express = require('express');
 var app = express();
 
-app.set('port', (process.env.port || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
