8484d99441cc8bbe8f29d02297b74696b2a6c24c
netreptodd <todd.nri.michael@gmail.com>
2016-12-26 16:53:54 -0500

    first check in
    supreme-adventure project creation
    nodeJS packages

---
+++ supreme-adventure/server.js
@@ -0,0 +1,17 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+
+//var express = require('express')
+//var app = express()
+
+//app.get('/', function (req, res) {
+//    res.send('Hello World!')
+//})
+
+//app.listen(3000, function () {
+//    console.log('Example app listening on port 3000!')
+//})
