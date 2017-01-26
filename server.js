276f6ea03fd1d98e2690dee8ac2a238e27f00f63
Steven Martin <steven@stevenmartin.io>
2017-01-26 07:17:02 -0500

    Need to post instead of get for Alexa

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res){
+  res.send('No GETs here...');
+});
+
+app.post('/', function (req, res) {
   res.json({
     "version": "1.0",
     "response": {
