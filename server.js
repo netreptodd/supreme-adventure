f56a3f9398c4201c828d2dbe74f5108451884204
Todd Michael <todd.nri.michael@gmail.com>
2017-01-25 20:41:38 -0500

    response
    response check in

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.post('/', function (req, res) {
+    //res.send('Hello World!');
+      res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": true,
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": "<speak>Hello my gods!</speak>"
+        }
+      }
+    });
 });
+
+
 
 app.listen(app.get('port'), function () {
     console.log('Example app listening on port ' + app.get('port') + '!');
