f5098b0528ca03e600b449a0cb0a87da1f201888
Steven Martin <steven@stevenmartin.io>
2017-02-08 20:07:51 -0500

    Fix linting errors

--- apps/supreme-adventure/gulpfile.js
+++ apps/supreme-adventure/gulpfile.js
@@ -2,13 +2,17 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
 
+var options = {
+  jsPaths: ['**/*.js', '!node_modules/**']
+};
+
 gulp.task('test', () => {
-  gulp.src('**/*.test.js', {read: false})
+  gulp.src(options.jsPaths, {read: false})
     .pipe(mocha());
 });
 
 gulp.task('lint', () => {
-  return gulp.src(['**/*.js', '!node_modules/**'])
+  return gulp.src(options.jsPaths)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
--- apps/supreme-adventure/index.js
+++ apps/supreme-adventure/index.js
@@ -1,12 +1,13 @@
-var alexa = require("alexa-app");
-var app = new alexa.app("sample");
+var alexa = require('alexa-app');
+var app = new alexa.app('sample');
 
-app.intent("number", {
-    "slots": { "number": "NUMBER" },
-    "utterances": ["say the number {1-100|number}"]
+app.intent('number',
+  {
+    'slots': { 'number': 'NUMBER' },
+    'utterances': ['say the number {1-100|number}']
   },
   function(request, response) {
-    var number = request.slot("number");
-    response.say("You asked for the number " + number);
+    var number = request.slot('number');
+    response.say('You asked for the number ' + number);
   }
 );
