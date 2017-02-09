cef947603cf11e18a5ab793726d44ed57fb6ed89
Steven Martin <steven@stevenmartin.io>
2017-02-08 21:43:42 -0500

    Bootstrap supreme adventure

--- apps/supreme-adventure/index.js
+++ apps/supreme-adventure/index.js
@@ -2,15 +2,20 @@ module.change_code = 1;
 var alexa = require('alexa-app');
 var app = new alexa.app('supreme-adventure');
 
-app.intent('numberIntent',
-  {
-    'slots': { 'number': 'NUMBER' },
-    'utterances': ['say the number {1-100|number}']
-  },
-  function(request, response) {
-    var number = request.slot('number');
-    response.say('You asked for the number ' + number);
-  }
-);
+app.launch((request, response) => {
+  response.say('Welcome to Supreme Adventure! Would you like to play a game?');
+  response.shouldEndSession(false);
+});
+
+app.intent('Help', {
+  'utterances': [
+    'help {|me}'
+  ]
+}, (request, response) => {
+  reponse.say('Here is a brief list of action words you can use to interact with the game. Attack, get, look, go, open, ask, bamboozle');
+});
+
+app.sessionEnded((request, response) => {
+});
 
 module.exports = app;
